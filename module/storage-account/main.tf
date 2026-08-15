98c9503a274943c8a4bba7dc75f39a286f820507
git config --global user.name <mritunjaykumar126@gmail.com>
2026-08-15 17:20:36 +0530

    Fix Terraform Azure pipeline & main.tf

--- module/storage-account/main.tf
+++ module/storage-account/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_storage_account" "strg" {
 
 for_each = var.strg-name
 
-  name=teststrg126
+  name=each.value.name
   resource_group_name = each.value.resource_group_name
   location=each.value.location
   account_tier = each.value.account_tier
